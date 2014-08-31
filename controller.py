dea32698637a90364bdad1ef506760c307ae5605
João Pinto <lamego.pinto@gmail.com>
2014-08-31 18:17:31 +0100

    Added cookie handling functions to the controller module

--- controller.py
+++ controller.py
@@ -10,7 +10,7 @@ def add_static_dir(application, static_dir, dir_name):
     application.merge(conf)
 
 
-def attach(controller_obj, controller_path_name):
+def attach(controller_path_name, controller_obj,):
     global root
     controller_path_name = controller_path_name.strip("/")
     if controller_path_name == '':  # No path provided, override the index handler
@@ -18,5 +18,29 @@ def attach(controller_obj, controller_path_name):
     else:
         setattr(root, controller_path_name, controller_obj)
 
+
 def publish(func=None, *args):
     return cherrypy.expose(func, *args)
+
+
+def get_cookie(name, default=None):
+    return cherrypy.request.cookie.get(name, default)
+
+
+def set_cookie(name, value, path='/', max_age=3600, version=1):
+    cookie = cherrypy.response.cookie
+    cookie[name] = value
+    cookie[name]['path'] = path
+    cookie[name]['max-age'] = max_age
+    cookie[name]['version'] = version
+
+
+def delete_cookie(name):
+    cherrypy.response.cookie[name] = 'deleting'
+    cherrypy.response.cookie[name]['expires'] = 0
+
+def method():
+    return cherrypy.request.method
+
+def params()    :
+    return cherrypy.request.params
